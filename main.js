661a772fa15d100157ad67ac8a184cb43d2c18e6
mohneel <mohneelp@gmail.com>
2019-12-04 21:14:45 -0800

    a

--- main.js
+++ main.js
@@ -23,17 +23,22 @@
 
 
     function makeScatterPlot() {
-        console.log(data.mean);
-        let Country = data.map((row) => (row["Country"]))
+        console.log(data);
+        let Country = data.map((row) => ((row["Gender Development Index value"])))
         console.log(Country);
 
         let temp = ["(all)"]
         Country = temp.concat(Country.filter( onlyUnique )); 
-        const eachYear = function(d) {return d};
-
-        // let Legendary =  data.map((row) => (row["Legendary"]))
-        // Legendary = temp.concat(Legendary.filter( onlyUnique )); 
-        // const gen = function(d) {return d};
+        let Countrylen = Country.length;
+        let countryR = [];
+        countryR.push(temp);
+        for (let i = 30; i < Country.length; i+=50) {
+            countryR.push( "[" + i + "-" + (i + 50) + "]");
+            console.log(3,countryR)
+        }
+        const eachYear = function(d) {
+            return d
+        };
 
         div = d3.select("body").append("div")
                     .attr("class", "tooltip")
@@ -41,35 +46,19 @@
 
         d3.select("#selectButton")
             .selectAll('myOptions')
-            .data(Country)
+            .data(countryR)
             .enter()
             .append('option')
             .attr('value',eachYear)
             .text(eachYear)
 
-        // d3.select("#selectButton2")
-        //     .selectAll('myOptions')
-        //     .data(Legendary)
-        //     .enter()
-        //     .append('option')
-        //     .attr('value',gen)
-        //     .text(gen)
-
         d3.select("#selectButton").on("change", function(d) {
             let selectedOption = d3.select(this).property("value")
-            // let selectedOption2 = d3.select("#selectButton2").property("value")
             update(selectedOption, data)
         })
 
-        // d3.select("#selectButton2").on("change", function(d) {
-        //     let selectedOption2 = d3.select(this).property("value")
-        //     let selectedOption = d3.select("#selectButton").property("value")
-        //     update(selectedOption, selectedOption2 , data)
-        // })
-
         let Sp_def = data.map((row) => parseInt(row["Life expectancy"]))
         let Total = data.map((row) =>  parseFloat(row["Total Population (millions) 2015"]))
-        // console.log(Total)
 
         function onlyUnique(value, index, self) { 
             return self.indexOf(value) === index;
@@ -79,21 +68,31 @@
         const limits = findMinMax(Sp_def, Total)              
 
         let scaleX = d3.scaleLinear()
-            .domain([0, 100])
+            .domain([10, 100])
             .range([0 + measurements.marginAll, measurements.width - measurements.marginAll])
 
         let scaleY = d3.scaleLinear()
             .range([measurements.height, 0])
-            .domain([-100, 250]); 
+            .domain([-40, 300]); 
         drawAxes(scaleX, scaleY)
         update("(all)", data);
         function update(selectedGroup, data) {
             let dataFilter;
+            // selectedGroup = selectedGroup.slice(0[,2]);
+            console.log(1,selectedGroup)
+            let a, b;
+            if ("[30-80]" === selectedGroup) { a = 30, b = 80};
+            if ("[80-130]" === selectedGroup) { a = 80, b = 130};
+            // if ("[100-150]" === selectedGroup) { a = 100, b = 150};
+            a = a * 0.01;
+            b =  b * 0.01;
             if (selectedGroup === "(all)") {
                 dataFilter = data;
             } else {
-                dataFilter = data.filter(function(d){return d.Country==selectedGroup});
+                dataFilter = data.filter(function(d)
+                {return (d["Gender Development Index value"] > (a + "") && d["Gender Development Index value"] < (b + ""))});
             }
+            console.log(1,a)
             Sp_def = dataFilter.map((row) => parseInt(row["Life expectancy"]))
             Total = dataFilter.map((row) =>  parseFloat(row["Total Population (millions) 2015"]))
             plotData(scaleX, scaleY, dataFilter)
@@ -146,18 +145,14 @@
                 .attr('cx', xMap)
                 .attr('cy', yMap)
                 .attr('r', 5)
-                .attr('fill', "#4E79A7"
-                
-                
-                
+                .attr('fill', "#4E79A7"                
                 )
                 .on("mouseover", (d) => {
                     div.transition()
                         .duration(200)
                         .style("opacity", .9);
-                    div.html(d["Name"] + "<br/>" + 
-                             d["Type 1"] + "<br/>" +
-                             d["Type 2"] + "<br/>")
+                    div.html(d["Country"] + "<br/>" + 
+                        "GDI:- "+d["Gender Development Index value"] + "<br/>")
                         .style("left", (d3.event.pageX) + "px")
                         .style("top", (d3.event.pageY) + "px");
                     })
@@ -178,6 +173,13 @@
           .text('Life expectancy')
           .attr('class', "label");
     
+        svgContainer.append('text')
+          .attr('x', 450)
+          .attr('y', 20)
+          .style('font-size', '14pt')
+          .text('Total Population vs Life expectancy')
+          .attr('class', "label");
+
         svgContainer.append('text')
           .attr('transform', 'translate(15, 300)rotate(-90)')
           .style('font-size', '14pt')
